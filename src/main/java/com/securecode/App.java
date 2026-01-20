10c002d7ec9d0328249634a55624b00dc907eaa3
Sakthi <82046832+0xsakthi@users.noreply.github.com>
2026-01-21 00:59:40 +0530

    Create secure application entry point example

---
+++ src/main/java/com/securecode/App.java
@@ -0,0 +1,9 @@
+package com.securecode;
+
+public class App {
+    public static void main(String[] args) {
+        // Secure application entry point example
+        System.out.println("Hello, Secure World!");
+        // Additional security measures can be implemented here
+    }
+}
